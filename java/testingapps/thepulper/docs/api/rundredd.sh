19edbfdc44b62b34be6b0ff269d0e4f991d7c0dd
alan <alan@compendiumdev.co.uk>
2019-07-06 14:47:04 +0100

    more examples

--- java/testingapps/thepulper/docs/api/rundredd.sh
+++ java/testingapps/thepulper/docs/api/rundredd.sh
@@ -1,1 +1,1 @@
-dredd pulper-openapi.yml http://localhost:4567 --header="X-API-AUTH: 99ae7a53-c657-4034-b542-b384dfa14da3" > exec.txt
+dredd pulper-openapi.yml http://localhost:4567 --header="X-API-AUTH: 2a982807-15b1-4824-a25c-6de88e337600" > exec.txt
