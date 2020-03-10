a17883573b31f4604dfaee32119db7048c1b6cd4
alan <alan@compendiumdev.co.uk>
2020-03-10 16:28:01 +0000

    added agile stories and search to the api collections and docs

--- java/testingapps/thepulper/docs/general/build/makepdfs.sh
+++ java/testingapps/thepulper/docs/general/build/makepdfs.sh
@@ -1,3 +1,3 @@
 pandoc ../overview.md -f markdown -t latex --toc -o ./export/exercises.pdf
 pandoc ../kbs.txt -f markdown -t latex --toc -o ./export/knownbugs.pdf
-pandoc -s ../overview.md ../kbs.txt -f markdown -t latex --toc -o ./export/knownbugs.pdf
+pandoc -s ../overview.md ../kbs.txt -f markdown -t latex --toc -o ./export/releases.pdf
