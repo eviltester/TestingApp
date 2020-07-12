d7c494838724705f324f414091d9794cc59a6888
alan <alan@compendiumdev.co.uk>
2020-07-12 12:53:07 +0100

    trying to fix links and css via script

--- deploy-web-pages-as-github-pages.sh
+++ deploy-web-pages-as-github-pages.sh
@@ -1,6 +1,7 @@
 cp -R java/testingapps/compendiumdevapps/src/main/resources/web/apps docs/apps
 cp -R java/testingapps/compendiumdevapps/src/main/resources/web/games docs/games
 cp -R java/testingapps/shared-styles/src/main/resources/web/css docs/css
-# remember to test
-# in docs
-# php -S localhost:8000
+cd docs
+find . -name '*.html' |xargs perl -pi -e 's/\"\/css\//\"https:\/\/eviltester\.github\.io\/TestingApp\/css\//g'
+find . -name '*.html' |xargs perl -pi -e 's/\"\/games\//\"https:\/\/eviltester\.github\.io\/TestingApp\/games\//g'
+find . -name '*.html' |xargs perl -pi -e 's/\"\/apps\//\"https:\/\/eviltester\.github\.io\/TestingApp\/apps\//g'
