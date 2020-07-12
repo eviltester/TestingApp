40c51b70107245dc8a92c01c18882e94d7a189f6
alan <alan@compendiumdev.co.uk>
2020-07-12 13:32:01 +0100

    index updates

--- deploy-web-pages-as-github-pages.sh
+++ deploy-web-pages-as-github-pages.sh
@@ -2,6 +2,7 @@ cp -R java/testingapps/compendiumdevapps/src/main/resources/web/apps docs/apps
 cp -R java/testingapps/compendiumdevapps/src/main/resources/web/games docs/games
 cp -R java/testingapps/shared-styles/src/main/resources/web/css docs/css
 cd docs
-find . -name '*.html' |xargs perl -pi -e 's/\"\/css\//\"https:\/\/eviltester\.github\.io\/TestingApp\/css\//g'
-find . -name '*.html' |xargs perl -pi -e 's/\"\/games\//\"https:\/\/eviltester\.github\.io\/TestingApp\/games\//g'
-find . -name '*.html' |xargs perl -pi -e 's/\"\/apps\//\"https:\/\/eviltester\.github\.io\/TestingApp\/apps\//g'
+find . -name '*.htm*' |xargs perl -pi -e 's/\"\/css\//\"https:\/\/eviltester\.github\.io\/TestingApp\/css\//g'
+find . -name '*.htm*' |xargs perl -pi -e 's/\"\/games\//\"https:\/\/eviltester\.github\.io\/TestingApp\/games\//g'
+find . -name '*.htm*' |xargs perl -pi -e 's/\"\/apps\//\"https:\/\/eviltester\.github\.io\/TestingApp\/apps\//g'
+find . -name '*.htm*' |xargs perl -pi -e 's/\"\/index.html/\"https:\/\/eviltester\.github\.io\/TestingApp\/index.html/g'
