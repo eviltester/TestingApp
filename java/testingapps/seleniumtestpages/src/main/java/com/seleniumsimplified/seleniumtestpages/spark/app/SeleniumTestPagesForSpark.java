28ada2c618800fa7883eb7b3bbe58a88efdabc01
alan <alan@compendiumdev.co.uk>
2023-07-25 17:32:50 +0100

    easier to replace hide function, text desc changes and an id change in scribble. deprecated unstyled.

--- java/testingapps/seleniumtestpages/src/main/java/com/seleniumsimplified/seleniumtestpages/spark/app/SeleniumTestPagesForSpark.java
+++ java/testingapps/seleniumtestpages/src/main/java/com/seleniumsimplified/seleniumtestpages/spark/app/SeleniumTestPagesForSpark.java
@@ -24,19 +24,24 @@ public class SeleniumTestPagesForSpark {
         post("/selenium/calculate.php", (req, res) -> {return new PhpCalculate(req,res).post();});
         get("/selenium/refresh.php", (req, res) -> {return new PhpRefresh(req,res).get();});
         post("/selenium/form_processor.php", (req, res) -> {return new PhpFormProcessor(req,res).post();});
-        get("/selenium/find_by_playground.php", (req, res) -> {return new ResourceReader().asString("/web/find_by_playground.html");});
+        post("/legacy/form_processor.php", (req, res) -> {return new PhpFormProcessor(req,res).post();});
+        get("/selenium/find_by_playground.php", (req, res) -> {return new ResourceReader().asString("/web/legacy/find_by_playground.html");});
 
         // some tests check the url of the asked for page so don't redirect this one
-        get("/selenium/basic_web_page.html", (req, res) -> {return new ResourceReader().asString("/web/basic_web_page.html");});
-        get("/selenium/gui_user_interactions.html", (req, res) -> {return new ResourceReader().asString("/web/gui_user_interactions.html");});
-
+        get("/selenium/basic_web_page.html", (req, res) -> {return new ResourceReader().asString("/web/legacy/basic_web_page.html");});
+        get("/selenium/gui_user_interactions.html", (req, res) -> {return new ResourceReader().asString("/web/legacy/gui_user_interactions.html");});
 
         get("/ajaxselect.php", (req, res) -> {return new PhpAjaxSelect(req,res).get();});
+        get("/legacy/ajaxselect.php", (req, res) -> {return new PhpAjaxSelect(req,res).get();});
         get("/calculate.php", (req, res) -> {return new PhpCalculate(req,res).get();});
+        get("/legacy/calculate.php", (req, res) -> {return new PhpCalculate(req,res).get();});
         post("/calculate.php", (req, res) -> {return new PhpCalculate(req,res).post();});
+        post("/legacy/calculate.php", (req, res) -> {return new PhpCalculate(req,res).post();});
         get("/refresh.php", (req, res) -> {return new PhpRefresh(req,res).get();});
+        get("/legacy/refresh.php", (req, res) -> {return new PhpRefresh(req,res).get();});
         post("/form_processor.php", (req, res) -> {return new PhpFormProcessor(req,res).post();});
-        get("/find_by_playground.php", (req, res) -> {return new ResourceReader().asString("/web/find_by_playground.html");});
+        get("/find_by_playground.php", (req, res) -> {return new ResourceReader().asString("/web/legacy/find_by_playground.html");});
+        get("/legacy/find_by_playground.php", (req, res) -> {return new ResourceReader().asString("/web/legacy/find_by_playground.html");});
 
         // pretty versions
         get("/styled/calculator", (req, res) -> {return new PhpPrettyCalculate(req,res).get();});
@@ -69,6 +74,8 @@ public class SeleniumTestPagesForSpark {
         get("/selenium/search.php", (req, res) -> {return new PhpSearch(req,res).get();});
         post("/search.php", (req, res) -> {return new PhpSearch(req,res).post();});
         get("/search.php", (req, res) -> {return new PhpSearch(req,res).get();});
+        post("/legacy/search.php", (req, res) -> {return new PhpSearch(req,res).post();});
+        get("/legacy/search.php", (req, res) -> {return new PhpSearch(req,res).get();});
         // I Was going to rewrite the find_by_playground.php but then realised that the html is actually static now
         // as I just added the output of the old php to the resources and serve it up as a static html page
         //get("/find_by_playground.php", (req, res) -> {return new PhpFindByPlayground(req,res).get();});
